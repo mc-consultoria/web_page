c1759f5744d600485b77b6c0a2504f40109faf88
Sofia <67163440+sofia974@users.noreply.github.com>
2025-09-18 11:17:06 -0500

    Configuro Vite para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
-
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
+  // 👇 base debe coincidir con el nombre exacto del repo
+  base: "/web_page/",
   plugins: [react(), tailwindcss()],
 });
