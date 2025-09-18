b2b9374c424676f6cafc292975e1a464902ca03a
Sofia <67163440+sofia974@users.noreply.github.com>
2025-09-18 13:31:23 -0500

    fix: production - dominio propio

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "/web_page/", 
+  base: "/",                 
   plugins: [react(), tailwindcss()],
 });
+
